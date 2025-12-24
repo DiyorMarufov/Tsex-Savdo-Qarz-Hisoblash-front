b76581095a42fc93a5cfb52c9a8ee540be0feb36
DiyorMarufov <marufovdiyor9@gmail.com>
2025-12-24 13:29:03 +0500

    pages/superadmin/reports changed

--- src/pages/superadmin/reports/CustomersReportPage.tsx
+++ src/pages/superadmin/reports/CustomersReportPage.tsx
@@ -1,12 +1,12 @@
 import { memo } from "react";
 import CustomersReportBalances from "../../../widgets/reports/CustomersReport/CustomersReportBalances/CustomersReportBalances";
-import ReportFilter from "../../../widgets/reports/SalesReport/ReportFilter/ReportFilter";
+// import ReportFilter from "../../../widgets/reports/SalesReport/ReportFilter/ReportFilter";
 import CustomersReportChart from "../../../widgets/reports/CustomersReport/CustomersReportChart/CustomersReportChart";
 
 const CustomersReportPage = () => {
   return (
     <div className="flex flex-col gap-5">
-      <ReportFilter />
+      {/* <ReportFilter /> */}
       <CustomersReportBalances />
 
       <CustomersReportChart />
--- src/pages/superadmin/reports/TsexesReportPage.tsx
+++ src/pages/superadmin/reports/TsexesReportPage.tsx
@@ -1,12 +1,12 @@
 import { memo } from "react";
-import ReportFilter from "../../../widgets/reports/SalesReport/ReportFilter/ReportFilter";
+// import ReportFilter from "../../../widgets/reports/SalesReport/ReportFilter/ReportFilter";
 import TsexesReportBalances from "../../../widgets/reports/TsexesReport/TsexesReportBalances/TsexesReportBalances";
 import TsexesReportChart from "../../../widgets/reports/TsexesReport/TsexesReportChart/TsexesReportChart";
 
 const TsexesReportPage = () => {
   return (
     <div className="flex flex-col gap-5">
-      <ReportFilter />
+      {/* <ReportFilter /> */}
 
       <TsexesReportBalances />
 
