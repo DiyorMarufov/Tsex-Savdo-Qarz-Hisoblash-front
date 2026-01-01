7da781a62d041e6673c8f8aa3e1311d62b033844
DiyorMarufov <marufovdiyor9@gmail.com>
2026-01-01 15:21:11 +0500

    pages changed

--- src/pages/superadmin/reports/TsexesReportPage.tsx
+++ src/pages/superadmin/reports/TsexesReportPage.tsx
@@ -6,22 +6,36 @@ import { useParamsHook } from "../../../shared/hooks/params/useParams";
 import type { QueryParams } from "../../../shared/lib/types";
 import dayjs from "dayjs";
 import { useTsex } from "../../../shared/lib/apis/tsexes/useTsex";
+import ProTable from "@ant-design/pro-table";
+import { debounce } from "../../../shared/lib/functions/debounce";
+import { tsexColumns } from "../tsexes/model/tsexes-model";
+import { useNavigate } from "react-router-dom";
+import TsexesMobileList from "../../../widgets/tsexes/TsexesMobileList/TsexesMobileList";
+import SearchInput from "../../../shared/ui/SearchInput/SearchInput";
 
 const TsexesReportPage = () => {
   const [isTsexOpen, setIsTsexOpen] = useState<boolean>(false);
-  const { setParams, getParam } = useParamsHook();
-  const { getAllTsexesForProductsFilter } = useTsex();
+  const navigate = useNavigate()
+  const { setParams, getParam, removeParam } = useParamsHook();
+  const [localSearch, setLocalSearch] = useState(getParam("search") || "");
+  const { getAllTsexes, getAllTsexesForProductsFilter } = useTsex();
   const { getAllTsexesSummaryForReport, getAllTsexesStatisticsForReport } =
     useTsex();
 
   // Query starts
   const query: QueryParams = useMemo(() => {
+    const page = Number(getParam("page")) || 1;
+    const limit = Number(getParam("limit")) || 5;
+    const search = getParam("search") || undefined;
     const s = getParam("startDate");
     const e = getParam("endDate");
     const tsexId = getParam("tsexId") || "";
 
     const isFirstLoad = s === null && e === null;
     return {
+      page,
+      limit,
+      search,
       start: isFirstLoad ? dayjs().startOf("day") : s ? dayjs(s) : null,
       end: isFirstLoad ? dayjs().endOf("day") : e ? dayjs(e) : null,
       startStr: isFirstLoad
@@ -77,19 +91,79 @@ const TsexesReportPage = () => {
   // TsexesReportChart ends
 
   // TsexReport options start
-  const { data: tsexes, isLoading: tsexLoading } =
+  const { data: tsexesFilter, isLoading: tsexFilterLoading } =
     getAllTsexesForProductsFilter(isTsexOpen);
   const tsexesOptions = [
     {
       value: "",
       label: "Barcha tsexlar",
     },
-    ...(tsexes?.data?.map((ts) => ({
+    ...(tsexesFilter?.data?.map((ts) => ({
       value: ts?.id,
       label: ts?.name,
     })) || []),
   ];
   // TsexReport options ends
+
+  // HandleOpenDetail starts
+  const handleOpenDetail = (id: string) => {
+    navigate(`/superadmin/tsexes/transactions/${id}`);
+  };
+  // HanleOpenDetail ends
+
+
+  // TsexData starts
+  const { data: allTsexes, isLoading: tsexLoading } = getAllTsexes({
+    startDate: query.startStr,
+    endDate: query.endStr,
+    tsexId: query.tsexId,
+    search: query.search,
+    page: query.page,
+    limit: query.limit
+  });
+  const tsexes = allTsexes?.data?.data;
+  const total = allTsexes?.data?.total || 0;
+  // TsexData ends
+
+  // PageChange starts
+  const handlePageChange = (newPage: number, newPageSize?: number) => {
+    const updateParams: { page?: number; limit?: number } = {};
+
+    if (newPage > 1) {
+      updateParams.page = newPage;
+    }
+
+    if (newPageSize && newPageSize !== 5) {
+      updateParams.limit = newPageSize;
+    }
+
+    setParams(updateParams);
+
+    if (newPage === 1) {
+      removeParam("page");
+    }
+    if (newPageSize === 5 && getParam("limit")) {
+      removeParam("limit");
+    }
+  };
+  // PageChange ends
+
+  // Search starts
+  const debouncedSetSearchQuery = useCallback(
+    debounce((nextValue: string) => {
+      setParams({
+        search: nextValue || "",
+        page: 1,
+      });
+    }, 500),
+    [setParams],
+  );
+
+  const handleSearchChange = (value: string) => {
+    setLocalSearch(value);
+    debouncedSetSearchQuery(value);
+  };
+  // Search ends
 
   return (
     <div className="flex flex-col gap-5">
@@ -99,7 +173,7 @@ const TsexesReportPage = () => {
         end={query.end}
         tsexId={query.tsexId}
         tsexesOptions={tsexesOptions}
-        tsexLoading={tsexLoading}
+        tsexLoading={tsexFilterLoading}
         setIsTsexOpen={setIsTsexOpen}
       />
 
@@ -113,6 +187,46 @@ const TsexesReportPage = () => {
       />
 
       <TsexesReportChart data={tsexesStats} loading={tsexStatsLoading} />
+
+      <div className="rounded-[12px] border border-e-bg-fy bg-[#ffffff] p-3.5 flex items-center gap-5 max-[960px]:flex-wrap">
+        <SearchInput
+          placeholder="Tsex nomi yoki operatsiya bo'yicha qidirish"
+          className="h-12! bg-bg-ty! text-[17px]!"
+          value={localSearch}
+          onChange={handleSearchChange}
+        />
+      </div>
+
+      <div className="max-[500px]:hidden">
+        <ProTable
+          dataSource={tsexes}
+          rowKey="id"
+          pagination={{
+            showSizeChanger: true,
+            responsive: false,
+            current: query.page,
+            pageSize: query.limit,
+            total,
+            onChange: handlePageChange,
+            pageSizeOptions: [5, 10],
+          }}
+          columns={tsexColumns(handleOpenDetail)}
+          search={false}
+          dateFormatter="string"
+          scroll={{ x: "max-content" }}
+          loading={tsexLoading}
+        />
+      </div>
+
+      <TsexesMobileList
+        data={tsexes}
+        currentPage={Number(query.page)}
+        pageSize={Number(query.limit)}
+        total={total}
+        onPageChange={handlePageChange}
+        loading={tsexLoading}
+        onDetail={handleOpenDetail}
+      />
     </div>
   );
 };
